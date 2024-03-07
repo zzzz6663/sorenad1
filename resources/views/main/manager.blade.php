ea5b7837b4dcc597f9de1091a09afa5ac4abb105
naser <na3r.jafari@gmail.com>
2024-03-07 18:25:04 +0330

    es172

--- resources/views/main/manager.blade.php
+++ resources/views/main/manager.blade.php
@@ -113,7 +113,7 @@
     <script src="{{ asset('/admin/assets/js/persian-datepicker.js') }}"></script>  --}}
     {{--  <script src="{{ asset('/home/js/jquery.mmenu.min.all.js') }}"></script>  --}}
     {{--  <script type="text/javascript" src="{{ asset('/common/tinymce/tinymce.min.js') }}"></script>  --}}
-    {{--  <script src="{{ asset('/admins/') }}"></script>  --}}
+    {{--  <script src="{{ asset('/sadmins/') }}"></script>  --}}
     @vite( 'resources/js/app.js')
 
 
