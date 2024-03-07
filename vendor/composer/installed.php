ce56bc3670bf7daf82d593f45079be5d09306def
naser <na3r.jafari@gmail.com>
2024-03-07 13:31:48 +0330

    es165

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'dfed1e0f65fea5e57684230d97d18c0e86d0ed0c',
+        'reference' => '13bd7acecc60e507696e9674c8860a7c289459c0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -412,7 +412,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'dfed1e0f65fea5e57684230d97d18c0e86d0ed0c',
+            'reference' => '13bd7acecc60e507696e9674c8860a7c289459c0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
